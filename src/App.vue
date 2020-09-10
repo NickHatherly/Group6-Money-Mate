9085766ba42b2ab1d2da72db8c9c94940e04fa28
HeroicNerd <19388104@student.westernsydney.edu.au>
2020-09-10 11:06:04 +1000

    updated code (float/Double)

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,101 @@
 <template>
-  <div id="app">
-        <input type="text" v-model="text"/>
-        <p></p>
-        <input type="double" v-model="float"/>
-        <p></p>
-        <v-row justify="center">
-        <v-date-picker v-model="picker"></v-date-picker>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-email</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Contact</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      color="indigo"
+      dark
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col class="text-center">
+              <template>
+                <v-text-field
+                label="Description"
+                single-line v-model="text"></v-text-field>
+                <v-text-field
+                label="Amount"
+                single-line v-model="double"></v-text-field>
+                <v-row>
+                <v-date-picker v-model="picker"></v-date-picker>
+                </v-row>
+                <v-btn  @click="addtransaction ()">Save Transaction</v-btn>
+              </template>
+          </v-col>
         </v-row>
-        <p></p>
-        <button @click="addtransaction ()">Save Transaction</button>
-  </div>
+      </v-container>
+    </v-main>
+    <v-footer
+      color="indigo"
+      app
+    >
+      <span class="white--text">&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-export default {
-  name: 'App',
-  data () {
-    return {
-      float: null,
+  export default {
+    props: {
+      source: String,
+    },
+    name: 'App',
+    data: () => {
+      return {
+      double: null,
       transactions: [],
-      date: null
+      date: null,
+      picker: null,
+      drawer: null
     }
-  },
+    },
 
   methods: {
     addtransaction () {
-      this.transactions.push(this.double)
-      this.float - null
       this.transactions.push(this.text)
       this.text - null
+      this.transactions.push(this.double)
+      this.double - null
       this.transactions.push(Math.round(+new Date()/1000))
-      this.text - null
+      this.double - null
+      this.transactions.push(this.picker)
       this.$localStorage.set('transactions' + " " + localStorage.length, JSON.stringify(this.transactions),)
       this.transactions = []
     }
   }
 }
-
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  float-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueLocalStorage from 'vue-localstorage'
+import vuetify from './plugins/vuetify';
 
 Vue.use(VueLocalStorage)
 Vue.config.productionTip = false
 
 new Vue({
   el: '#app',
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
 
