8d9991e1d6a14ea30e50776312ff713fea04c8bb
lugster <lucas.baldacchino@gmail.com>
2020-09-21 15:18:24 +1000

    Chips attempt

--- tesserttest/src/App.vue
+++ tesserttest/src/App.vue
@@ -2,14 +2,18 @@
   <div id="app">
     <button v-on:click="recognize">recognize</button>
     <img id="text-img" alt="Vue logo" src="./assets/low.jpg">
-                <v-chip-group
-                  multiple
-                  active-class="primary--text"
-                >
-                  <v-chip v-for="tag in tags" :key="tag">
-                    {{ tag }}
-                  </v-chip>
-                </v-chip-group>
+
+    <v-chip-group multiple active-class="primary--text">
+      <v-chip v-for="tag in tags" :key="tag">
+        {{ tag }}
+      </v-chip>
+    </v-chip-group>
+    <v-file-input
+      label="File input"
+      filled
+      prepend-icon="mdi-camera"
+    ></v-file-input>
+
   </div>
 </template>
 
@@ -22,45 +26,46 @@ const worker = createWorker({
 });
 
 export default {
-   props: {
-      source: String,
-    },
+  props: {
+    source: String,
+  },
   name: 'app',
-   data: () => {
-      return {
+  data: () => {
+    return {
       double: null,
       myResult: '',
       transactions: [],
-      
-
+      testtext: '',
+      tags: [] //tags is the array where chips data is kept
     }
-    },
-  methods: { 
-    
-  recognize: async () => {
-  const img = document.getElementById('text-img');
-  console.log(img);
-  await worker.load();
-  await worker.loadLanguage('eng');
-  await worker.initialize('eng', OEM.LSTM_ONLY);
-  await worker.setParameters({
-      tessedit_pageseg_mode: PSM.SINGLE_BLOCK,
+  },
+  methods: {
+
+    //tesseract data read
+    recognize: async () => {
+      var tags;
+      const img = document.getElementById('text-img');
+      console.log(img);
+      await worker.load();
+      await worker.loadLanguage('eng');
+      await worker.initialize('eng', OEM.LSTM_ONLY);
+      await worker.setParameters ({
+        tessedit_pageseg_mode: PSM.SINGLE_BLOCK,
       });
-  scheduler.addWorker(worker);
-  const { data: { text } } = await worker.recognize(img);
-  var mytext = JSON.stringify(text);
-  var textread = mytext.split(" ");
-  
- console.log(mytext);
-  
-  var i;
-  for (i = 0; i < textread.length; i++) {
-    console.log(textread[i])
-    
+      scheduler.addWorker(worker);
+      const {data: { text } } = await worker.recognize(img);//text it the output from the image
+      var mytext = JSON.stringify(text); //turn the text into a string
+      var textread = mytext.split(" "); //split the stringified text to be turrned into tokens
+      console.log(textread);
+
+      var i;
+      for (i = 0; i < textread.length; i++) {//goes through each token and seperates it into individual pieces of data 
+        console.log(textread[i])
       }
-  
-      
-    },  
+      this.textread = [tags] //this is where we are trying to place the data into the chips array but can't
+
+    },
+    
   }
 }
 
