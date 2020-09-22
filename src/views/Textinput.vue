c82cf74bc21121ba775be6999d0caeec610a07b0
HeroicNerd <19388104@student.westernsydney.edu.au>
2020-09-22 21:48:46 +1000

    drop down category

--- src/views/Textinput.vue
+++ src/views/Textinput.vue
@@ -1,47 +1,50 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-form v-model="isValid" cols="60" md="6">
-        <v-col>
+  <v-form v-model="isValid" cols="60" md="6">
+    <v-container>
+      <v-row>
+        <v-col cols="10" md="6">
           <v-text-field
             v-model="amount"
             label="Amount"
             :rules="[v => !!v || 'Please Enter an Amount']"
             required
           ></v-text-field>
+
+          <v-text-field v-model="description" label="Description"></v-text-field>
         </v-col>
-      </v-form>
-      <v-col cols="10" md="6"></v-col>
 
-      <v-col cols="10" md="6">
-        <v-text-field v-model="description" label="Description"></v-text-field>
-      </v-col>
+        <v-col cols="10" md="6">
+          <v-menu
+            ref="date"
+            v-model="menu"
+            :close-on-content-click="false"
+            :return-value.sync="date"
+            transition="scale-transition"
+            offset-y
+            min-width="290px"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-text-field v-model="date" label="Date" readonly v-bind="attrs" v-on="on"></v-text-field>
+            </template>
+            <v-date-picker v-model="date" no-title scrollable>
+              <v-spacer></v-spacer>
+              <v-btn text color="primary" @click="menu = false">Cancel</v-btn>
+              <v-btn text color="primary" @click="$refs.date.save(date)">OK</v-btn>
+            </v-date-picker>
+          </v-menu>
 
-      <v-col cols="10" md="6" lg="2">
-        <v-menu
-          ref="date"
-          v-model="menu"
-          :close-on-content-click="false"
-          :return-value.sync="date"
-          transition="scale-transition"
-          offset-y
-          min-width="290px"
-        >
-          <template v-slot:activator="{ on, attrs }">
-            <v-text-field v-model="date" label="Date" readonly v-bind="attrs" v-on="on"></v-text-field>
-          </template>
-          <v-date-picker v-model="date" no-title scrollable>
-            <v-spacer></v-spacer>
-            <v-btn text color="primary" @click="menu = false">Cancel</v-btn>
-            <v-btn text color="primary" @click="$refs.date.save(date)">OK</v-btn>
-          </v-date-picker>
-        </v-menu>
-      </v-col>
-      <v-col offset-md="1">
-        <v-btn :disabled="!isValid" @click="addTransaction()">Save json</v-btn>
-      </v-col>
-    </v-row>
-  </v-container>
+          <v-overflow-btn
+            class="my-2"
+            :items="dropdown_category"
+            label="Category"
+            target="#dropdown-example"
+          ></v-overflow-btn>
+
+          <v-btn :disabled="!isValid" @click="addTransaction()">Save json</v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
 </template>
 
 
@@ -51,6 +54,16 @@ import { mapState } from "vuex";
 
 export default {
   data: () => ({
+    dropdown_category: [
+      "Food",
+      "Entertainment",
+      "Bills",
+      "Utilities",
+      "Health",
+      "Transport",
+      "Shopping",
+      "groceries",
+    ],
     date: new Date().toISOString().substr(0, 10),
     menu: false,
     store,
